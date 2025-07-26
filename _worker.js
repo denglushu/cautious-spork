43704c1b75da8e02a24c3a961f63f1ca51aae71b
denglushu <dlushu@163.com>
2025-07-27 04:19:32 +0800

    更新 _worker.js

--- _worker.js
+++ _worker.js
@@ -5,20 +5,21 @@ addEventListener('fetch', event => {
 async function handleRequest(request) {
   const url = new URL(request.url);
   const inputPassword = url.searchParams.get('password') || '';
-
-  // 预设密码
   const correctPassword = "888";
 
   if (inputPassword !== correctPassword) {
     return new Response(JSON.stringify("密码错误"), {
       status: 401,
-      headers: { 'Content-Type': 'application/json' },
+      headers: { 
+        'Content-Type': 'application/json; charset=utf-8' // 修复乱码
+      },
     });
   }
 
-  // 返回水果数据
   const fruits = ["1636983341", "573776258", "mar4pis"];
   return new Response(JSON.stringify(fruits), {
-    headers: { 'Content-Type': 'application/json' },
+    headers: { 
+      'Content-Type': 'application/json; charset=utf-8' // 修复乱码
+    },
   });
 }
